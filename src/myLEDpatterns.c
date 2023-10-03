d93d340098d266f847047c71f51bec985f1336d4
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-10-02 19:17:36 -0600

    patterns(argp): implement custom help/version/usage messages
    argp makes "--help" use "-?" as its short option, which is
    understandable if the goal is to avoid eating "-h" by default. However,
    we do indeed want "-h" to mean "help".

--- src/myLEDpatterns.c
+++ src/myLEDpatterns.c
@@ -1,18 +1,51 @@
 // Lab 8: LED Patterns in C
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <argp.h>
 #include <time.h>
 
 
-// Version information
+// Argument parser metadata
 const char *argp_program_version = "myLEDpatterns 1.0";
-// Documentation
+const char *argp_program_bug_address = "lucas.ritzdorf@student.montana.edu";
 static char doc[] =
     "Display LED patterns on a dedicated hardware peripheral.\n";
-// Argument parser setup
-static struct argp argp = {0, 0, 0, doc};
+static char args_doc[] = "ARG1 ARG2";
+// Argument parser options
+static struct argp_option options[] = {
+    {"help",    'h', 0, 0, "show this help message", -1},
+    {"version", 'V', 0, 0, "show program version", -1},
+    {"usage",   'u', 0, 0, "show program usage summary", -1},
+    {0}
+};
+static error_t parse_opt(int, char *, struct argp_state *);
+// Final parser setup
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+// Argument parsing logic
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    switch (key) {
+        case 'h':
+            // help
+            argp_state_help(state, state->out_stream, ARGP_HELP_STD_HELP);
+            break;
+        case 'V':
+            // version
+            fprintf(state->out_stream, "%s\n", argp_program_version);
+            exit(0);
+            break;
+        case 'u':
+            // usage
+            argp_state_help(state, state->out_stream, ARGP_HELP_USAGE | ARGP_HELP_EXIT_OK);
+            break;
+        default:
+            // Unknown option
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
 
 
 // Interrupt flag setup and handling
@@ -29,7 +62,7 @@ int main(int argc, char **argv) {
     signal(SIGINT, sig_handler);
 
     // Parse arguments
-    argp_parse(&argp, argc, argv, 0, 0, 0);
+    argp_parse(&argp, argc, argv, ARGP_NO_HELP, 0, 0);
     printf("Received %d arguments\n", argc);
 
     // Loop until interrupted
