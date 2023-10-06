95ff04a5b76ddf92c890a906718ef35fdb4279be
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-10-06 14:41:09 -0600

    patterns(argp): clean up pattern arg parsing logic

--- src/myLEDpatterns.c
+++ src/myLEDpatterns.c
@@ -94,18 +94,16 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
                 }
                 // Parse remaining arguments
                 unsigned int arg_index;
-                unsigned int pattern_index = 0;
                 for (arg_index = state->next - 1; arg_index < state->argc - 1; arg_index = arg_index + 2) {
                     // Ensure we don't consume more steps than we can handle
                     if (arguments->pattern.num_steps >= MAX_STEPS) {
                         break;
                     }
                     // Capture binary pattern and corresponding delay
-                    arguments->pattern.steps [pattern_index] = (uint8_t)strtol(state->argv[arg_index],     NULL, 0);
-                    arguments->pattern.delays[pattern_index] = (int)    strtol(state->argv[arg_index + 1], NULL, 0);
+                    arguments->pattern.steps [arguments->pattern.num_steps] = (uint8_t)     strtol(state->argv[arg_index],     NULL, 0);
+                    arguments->pattern.delays[arguments->pattern.num_steps] = (unsigned int)strtol(state->argv[arg_index + 1], NULL, 0);
                     // Increment pattern step count
                     arguments->pattern.num_steps++;
-                    pattern_index++;
                 }
                 // Update parser state with the args we just consumed
                 state->next = arg_index;
