ea95144c2a67304c67a7e96d4575265f38e160bd
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-10-14 18:16:44 -0600

    driver: move register offsets into dedicated header

--- src/driver/hps_led_patterns.c
+++ src/driver/hps_led_patterns.c
@@ -14,14 +14,7 @@
 //-----------------------------------------------------------------------
 // DEFINE STATEMENTS
 //-----------------------------------------------------------------------
-// Define the Component Register Offsets
-#define REG0_HPS_LED_CONTROL_OFFSET 0x0
-#define REG1_LED_REG_OFFSET 0x4
-#define REG2_BASE_RATE_OFFSET 0x8
-
-// Memory span of all registers (used or not) in the
-// component hps_led_patterns
-#define SPAN 0x10
+#include "reg_offsets.h"
 
 
 //-----------------------------------------------------------------------
---
+++ src/driver/reg_offsets.h
@@ -0,0 +1,12 @@
+#ifndef REG_OFFSETS_H
+#define REG_OFFSETS_H
+
+// Define the Component Register Offsets
+#define REG0_HPS_LED_CONTROL_OFFSET 0x0
+#define REG1_LED_REG_OFFSET 0x4
+#define REG2_BASE_RATE_OFFSET 0x8
+
+// Memory span of all registers (used or not) in the component hps_led_patterns
+#define SPAN 0x10
+
+#endif
